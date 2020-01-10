f62f8cb7776a5c7b35ac4a3d5d4536bbe55d6110
MikiKru <michal_kruczkowski@o2.pl>
2020-01-10 16:38:07 +0100

    Zmiana od Michała

--- Program.cs
+++ Program.cs
@@ -8,12 +8,10 @@ namespace ConsoleApp2
 		{
 			Console.WriteLine("Hello World!");
 			Console.WriteLine("Nanananananana na naana");
-<<<<<<< HEAD
 			Console.WriteLine("3");
 			Console.WriteLine("4");
-=======
 			Console.WriteLine("Uwaga Uwaga nowa zmiana na stronie");
->>>>>>> d17144d58129a42b0ada30060646b5d6c5a554ce
+
 		}
 	}
 }
